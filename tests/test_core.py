4b75cac0d2621477db317a4a791ae0a043fe0a3d
veryacademy <veryacademy@gmail.com>
2021-08-04 23:07:10 +0100

    nc

---
+++ tests/test_core.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_main_resource():
+    response_auth = client.get("/")
+    assert response_auth.status_code == 200
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
